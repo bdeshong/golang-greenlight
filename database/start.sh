86441503ddc3a6ea1fad30e292bccbd0536f9387
Brian DeShong <brian@deshong.net>
2022-09-11 15:16:18 -0400

    Add database script.

---
+++ database/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+docker run \
+    -e POSTGRES_PASSWORD=greenlight \
+    -e PGDATA=/var/lib/postgresql/data/pgdata \
+	-v /Users/brian/code/golang-greenlight/database/pgdata:/var/lib/postgresql/data \
+    -p 5432:5432 \
+    --rm \
+    postgres
