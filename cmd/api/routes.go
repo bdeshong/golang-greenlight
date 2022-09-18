8066ee0d29d5410e65f38c61ba97fdae53c1780c
Brian DeShong <brian.deshong@emfi.co>
2022-09-18 12:50:45 -0400

    Through 10.2.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
@@ -24,5 +24,5 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
 	// Return the httprouter instance.
-	return router
+	return app.recoverPanic(router)
 }
